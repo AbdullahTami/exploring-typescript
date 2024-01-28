502bc119f7a74d57bc024c63b14c92ca8a7aca41
abdullahqayid <3boo0odq0v@gmail.com>
2024-01-28 19:44:48 +0300

    exploring unions and aliases

--- app.ts
+++ app.ts
@@ -1,18 +1,30 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
-  name: "Abdullah",
-  age: 23,
-  hobbies: ["Sports", "Cooking"],
-};
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
 
-let favoriteActivities: string[];
-favoriteActivities = ["Sports"];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.length);
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
+  let result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+  // if (resultConversion === "as-number") return +result;
+  // return result.toString();
 }
+
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
