e3142d998e6bf6f216f34bae1b2f7ecd5b6da929
abdullahqayid <3boo0odq0v@gmail.com>
2024-02-03 19:08:30 +0300

    creating and using autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,105 +1,8 @@
-function Logger(logString: string) {
-  return function (constructor: Function) {
-    console.log(logString);
-    console.log(constructor);
-  };
-}
-
-function WithTemplate(template: string, hookId: string) {
-  return function <T extends { new (...args: any[]): { name: string } }>(
-    originalConstructor: T
-  ) {
-    console.log("Rendering template");
-    return class extends originalConstructor {
-      constructor(..._: any[]) {
-        super();
-        const hookEl = document.getElementById(hookId);
-        if (hookEl) {
-          hookEl.innerHTML = template;
-          hookEl.querySelector("h1")!.textContent = this.name;
-        }
-      }
-    };
-  };
-}
-
-//! Decorator functions run bottom up
-
-@Logger("Logging Person")
-@WithTemplate("<h1>My Person Object</h1>", "app")
-class Person {
-  name = "Abdullah";
-  constructor() {
-    console.log("Creating person object...");
-  }
-}
-
-const pers = new Person();
-console.log(pers);
-
-function Log(target: any, propertyName: string | Symbol) {
-  console.log("Property decorator!!!");
-  console.log(target, propertyName);
-}
-
-function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log("Accessor decorator");
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
-
-function Log3(
-  target: any,
-  name: string | Symbol,
-  descriptor: PropertyDescriptor
-) {
-  console.log("Method decorator");
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
-
-function Log4(target: any, name: string | Symbol, position: number) {
-  console.log("Parameter decorator");
-  console.log(target);
-  console.log(name);
-  console.log(position);
-}
-
-class Product {
-  @Log
-  title: string;
-  private _price: number;
-
-  @Log2
-  set price(val: number) {
-    if (val > 0) {
-      this._price = val;
-    } else {
-      throw new Error("Invalid price!");
-    }
-  }
-
-  constructor(t: string, p: number) {
-    this.title = t;
-    this._price = p;
-  }
-
-  @Log3
-  getPriceWithTax(@Log4 tax: number) {
-    return this._price * (1 * tax);
-  }
-}
-
-const p1 = new Product("Book", 19);
-const p2 = new Product("Book", 29);
-
+// Auto bind decorator
 function AutoBind(_: any, _1: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
     configurable: true,
-    enumerable: false,
     get() {
       const boundFn = originalMethod.bind(this);
       return boundFn;
@@ -107,78 +10,52 @@ function AutoBind(_: any, _1: string, descriptor: PropertyDescriptor) {
   };
   return adjDescriptor;
 }
+//ProjectInput Class
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
-class Printer {
-  message = "This works!";
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    )! as HTMLInputElement;
+    this.configure();
+    this.attach();
+  }
 
   @AutoBind
-  showMessage() {
-    console.log(this.message);
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
 
-const p = new Printer();
-
-const button = document.querySelector("button")!;
-button.addEventListener("click", p.showMessage);
-interface ValidatorConfig {
-  [property: string]: {
-    [validatableProp: string]: string[]; // ["required", "positive"]
-  };
-}
-const registeredValidators: ValidatorConfig = {};
-function Required(target: any, propName: string) {
-  registeredValidators[target.constructor.name] = {
-    [propName]: ["required"],
-  };
-}
-function PositiveNumber(target: any, propName: string) {
-  registeredValidators[target.constructor.name] = {
-    [propName]: ["positive"],
-  };
-}
-
-function validate(obj: any) {
-  const objValidatorConfig = registeredValidators[obj.constructor.name];
-  if (!objValidatorConfig) {
-    return true;
-  }
-  for (const prop in objValidatorConfig) {
-    for (const validator of objValidatorConfig[prop]) {
-      switch (validator) {
-        case "required":
-          return !!obj[prop];
-        case "positive":
-          return obj[prop] > 0;
-      }
-    }
-  }
-  return true;
-}
-// button.addEventListener("click", p.showMessage.bind(p));
-
-class Course {
-  @Required
-  title: string;
-  @PositiveNumber
-  price: number;
-
-  constructor(t: string, p: number) {
-    this.title = t;
-    this.price = p;
-  }
-}
-
-const courseForm = document.querySelector("form")!;
-courseForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const titleEl = document.getElementById("title") as HTMLInputElement;
-  const priceEl = document.getElementById("price") as HTMLInputElement;
-
-  const title = titleEl.value;
-  const price = +priceEl.value;
-  const createdCourse = new Course(title, price);
-  if (!validate(createdCourse))
-    return alert("Invalid input, please try again!");
-  console.log(createdCourse);
-});
+const prjInput = new ProjectInput();
